bd7be8bf3a4638c20277975a5e59cb8e2c554950
Jay Mohile <jamohile@gmail.com>
2016-03-07 18:45:09 -0500

    First Upload of Tarline Source
    This is the initial, master upload of tarline source. It is based in
    Tarline APK version 03_07_2016.2, a stable alpha.

---
+++ Assets/Resources/Scripts/MainGameObstacleHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGameObstacleHandler : MonoBehaviour {
+	public bool beenDestroyed = false;
+	public bool forceDestroy = false;
+	// Use this for initialization
+	void Start () {
+		MainGameManager.last_obstacle_right = (float)(gameObject.transform.position.x + gameObject.GetComponentInChildren<SpriteRenderer> ().bounds.size.x / 2);
+		MainGameManager.current_potential_space = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((beenDestroyed == false && gameObject.transform.position.x < Camera.main.transform.position.x - MainGameManager.cameraSize.x/2 - gameObject.GetComponentInChildren<SpriteRenderer>().bounds.size.x/2)||forceDestroy == true){
+			MainGameManager.Number_Of_Obstacles -= 1;
+			MainGameManager.All_Obstacles.RemoveAt(MainGameManager.All_Obstacles.IndexOf(gameObject));
+			DestroyImmediate(gameObject);
+			//MainGameManager.All_Obstacles.TrimToSize();
+			beenDestroyed = true;
+		}
+	}
+}
